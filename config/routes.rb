4fcac8fdf30da9d21751d4b47a4625ce929d6f6d
akinori0714 <akinori_ichimura@yahoo.co.jp>
2020-02-16 04:29:56 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root "messages#index"
   resources :users
   resources :communities, only: [:index, :new, :show, :create] do
+    collection do
+      get 'add_user_to_community'
+    end
     resources :chats, only: [:index, :create]
   end
   resources :scouts
