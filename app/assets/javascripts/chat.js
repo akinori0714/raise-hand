bad411856fc438167171842553fde26b0bfc17bb
akinori0714 <akinori_ichimura@yahoo.co.jp>
2020-02-20 18:05:06 +0900

    修正

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,4 +1,5 @@
 $(function(){
+  last_chat_id = $('.chat:last').data("chat-id");
 
   function buildHTML(chat){
     var addImage = chat.image ? `<img class = "image_size", src="${chat.image}">` : '' ;
@@ -21,6 +22,29 @@ $(function(){
         </div>`
       return html;
   };
+
+  var reloadchats = function(){
+    last_chat_id = $('.chat:last').data('chat_id');
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/chatsとなるよう文字列を書く
+      url: "api/chats",
+      type: "get",
+      dataType: "json",
+      //dataオプションでリクエストに値を含める
+      data: {id: last_chat_id}
+    })
+    .done(function(chats){
+      var insertHTML = '';
+      $.each(chats, function(i, chat){
+        insertHTML += buildHTML(chat)
+        $('.chats').animate({ scrollTop: $('.chats')[0].scrollHeight});
+      });
+      $('.chats').append(insertHTML);
+    })
+    .fail(function(){
+    });
+  };
+  setInterval(reloadchats, 7000);
 
   $('#new_chat').on('submit', function(e){
     e.preventDefault();
