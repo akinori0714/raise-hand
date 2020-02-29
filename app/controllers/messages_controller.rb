547439b2c9368634c8b71a634132fd0779ba5c55
akinori0714 <akinori_ichimura@yahoo.co.jp>
2020-02-29 21:39:20 +0900

    トップページ修正しました

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,10 @@
 class MessagesController < ApplicationController
 
   def index
-  end
-
-  def show
+    @articles = Article.limit(3)
+    @communities = Community.limit(3)
+    @projects = Project.limit(3)
+    @users = User.limit(20)
   end
 
 end
