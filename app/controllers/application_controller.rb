8962a7d9ee78c86cbc9b45bfe12aa60154bfa5a3
akinori0714 <akinori_ichimura@yahoo.co.jp>
2020-03-22 21:55:03 +0900

    マイページ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :image, :sex, :age, :area, :skill1_id, :skill2_id, :skill3_id, :skill4_id, :skill5_id, :introduce, :performance, :category_id, 
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+    :name, :email, :image, :sex, :age, :area,
+    :skill1_id, :skill2_id, :skill3_id, :skill4_id, :skill5_id,
+    :skill1_period, :skill2_period, :skill3_period, :skill4_period, :skill5_period,
+    :introduce, :performance, :category_id, 
     want_to_meet:[],
     want_to_do:[],
     jointype:[]])
